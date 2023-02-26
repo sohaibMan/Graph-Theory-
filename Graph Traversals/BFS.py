681031e1796379195fd6143781279ec02873139b
Sohaib Manah <souhaibemanah@gmail.com>
2023-02-26 15:31:53 +0100

    bfs implementation

--- Graph Traversals/BFS.py
+++ Graph Traversals/BFS.py
@@ -1,9 +1,32 @@
+# Description: Graph Traversals
 from queue import Queue
-
+adj_list = {
+    'A': ['B', 'D'],
+    'B': ['A', 'C'],
+    'C': ['B'],
+    'D': ['A', 'E', 'F'],
+    'E': ['D', 'F', 'G'],
+    'F': ['D', 'H', 'E'],
+    'G': ['E', 'H'],
+    'H': ['F', 'G']
+}
 q = Queue()
-q.put(1)
-q.put(2)
-q.put(3)
-# 3 2 1
-print(q.get(), q.get(), q.get())
-# remove 1
+out_list = []
+visited = {}
+for key in adj_list.keys():
+    visited[key] = False
+
+
+def BFS(u):
+    q.put(u)
+    while not q.empty():
+        v = q.get()
+        out_list.append(v)
+        for n in adj_list[v]:
+            if not visited[n]:
+                visited[n] = True
+                q.put(n)
+
+
+BFS('A')
+print(out_list)
